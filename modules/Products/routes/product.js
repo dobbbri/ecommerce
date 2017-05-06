5db372e5aa469ea34e085c86b1d637d96533af1b
Robson Maia <robsonmvieira@gmail.com>
2017-05-06 14:29:23 -0300

    all routes implemented

--- modules/Products/routes/product.js
+++ modules/Products/routes/product.js
@@ -5,11 +5,12 @@ const Action = require('../controller/actions')
 
 router.get('/', Action.listar)
 
-router.post('/', (req, res) => {
-  Action.cadastrar(req, res)
-})
+router.post('/', Action.cadastrar)
+
 router.put('/:id', (req, res) => {
   Action.editar(req, res)
 })
-
+router.delete('/:id', (req, res) => {
+  Action.remover(req, res)
+})
 module.exports = router
